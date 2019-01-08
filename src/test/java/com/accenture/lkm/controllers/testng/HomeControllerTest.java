6d4f5ca6026072da1682fb64003ce269796676c2
SrishtiVK <srishti.vijoy.kumar@accenture.com>
2019-01-08 10:13:03 +0530

    changes done by srishti

--- src/test/java/com/accenture/lkm/controllers/testng/HomeControllerTest.java
+++ src/test/java/com/accenture/lkm/controllers/testng/HomeControllerTest.java
@@ -39,7 +39,7 @@ public class HomeControllerTest {
 	public void testHomeGreeting() {
 		HomeController hc = new HomeController();
 		ModelAndView mv = hc.home();
-		assertEquals("Greeting Missing!", "Welcome Srishti in the worlf of Jenkins2" , mv.getModelMap().get("greeting"));
+		assertEquals("Greeting Missing!", "Welcome Srishti in the world of Jenkins" , mv.getModelMap().get("greeting"));
 	}
 	
 	@AfterTest
